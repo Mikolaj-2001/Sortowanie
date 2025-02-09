e109aedc597720416be75ac4fc9451ff7852062a
Mikolaj <mgafiuk2001@wp.pl>
2025-02-09 03:13:40 +0100

    Gotowy

---
+++ JAVA/main.js
@@ -0,0 +1,23 @@
+const array = [12, 67, 34, 33.01, 24, 2, 56, 8, 0x10, 23.02, 78, 34, 1e2, 45, 67, 98, 1];
+
+function bubbleSorting(array) {
+    let n = array.length;
+
+    for (let i = 0; i < n; i++) {
+        let shifted = false;
+
+        for (let j = 0; j < n - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                let temp = array[j];
+
+                array[j] = array[j + 1];
+
+                array[j + 1] = temp
+            }
+        }
+    }
+
+    return array
+}
+
+console.log(bubbleSorting(array))
